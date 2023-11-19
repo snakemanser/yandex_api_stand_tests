96c070d99160d2119480daf56bdcee552d0e0f0f
best_friend <supir.gobl@yandex.ru>
2023-11-19 16:34:54 +0300

    1st commit

---
+++ configuration.py
@@ -0,0 +1,7 @@
+URL_SERVICE = "https://3159b1e4-b515-4099-b097-375c5ad5d434.serverhub.praktikum-services.ru"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+DOC_PATH = "/docs/"
+
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
